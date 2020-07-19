d7562e56bfb10ad6362fb0f727d852ccfdbe84ef
Murilo <muriloricci@gmail.com>
2020-07-19 20:00:13 +0200

    Add files via upload

--- src/Hooks.php
+++ src/Hooks.php
@@ -24,21 +24,21 @@ class Hooks {
 		}
 		$worlds = ["All", "Antica", "Assombra", "Astera", "Belluma", "Belobra", "Bona", "Calmera", "Carnera", "Celebra", "Celesta", "Concorda", "Cosera", "Damora", "Descubra", "Dibra", "Duna", "Emera", "Epoca", "Estela", "Faluna", "Ferobra", "Firmera", "Funera", "Furia", "Garnera", "Gentebra", "Gladera", "Harmonia", "Helera", "Honbra", "Impera", "Inabra", "Javibra", "Jonera", "Kalibra", "Kenora", "Lobera", "Luminera", "Lutabra", "Macabra", "Menera", "Mitigera", "Monza", "Nefera", "Noctera", "Nossobra", "Olera", "Ombra", "Pacera", "Pacembra", "Peloria", "Premia", "Pyra", "Quelibra", "Quintera", "Refugia", "Relania", "Relembra", "Secura", "Serdebra", "Serenebra", "Solidera", "Talera", "Torpera", "Tortura", "Unica", "Venebra", "Vita", "Vunira", "Wintera", "Xandebra", "Xylona", "Yonabra", "Ysolera", "Zenobra", "Zuna", "Zunera"];
 		if (!in_array($world, $worlds)) {
-			return '<div class="error">Mundo inexistente.</div>';
+			return '<div class="error">' . wfMessage('tibiahighscores-error-world')->text() . '</div>';
 		}
 		if (empty($vocation)) {
 			$vocation = 'any';
 		}
 		$vocations= ["any", "none", "druid", "knight", "paladin", "sorcerer"];
 		if (!in_array($vocation, $vocations)) {
-			return '<div class="error">Vocação inválida.</div>';
+			return '<div class="error">' . wfMessage('tibiahighscores-error-vocation')->text() . '</div>';
 		}
 		$vocationAddon = '';
 		if ($vocation != 'any') {
 			$vocationAddon = '/' . $vocation;
 		}
 		if ( !is_numeric( $amount ) || intval( $amount ) <= 0 ) {
-			return '<div class="error">Quantidade precisa ser um valor numérico positivo.</div>';
+			return '<div class="error">' . wfMessage('tibiahighscores-error-amount')->text() . '</div>';
 		}
 		
 		$cache = \ObjectCache::getInstance( CACHE_ANYTHING );
@@ -48,7 +48,7 @@ class Hooks {
 			$json = file_get_contents($url);
 			$data = json_decode($json, true);
 			$highscores = $data['highscores']['data'];
-			$table = '<table class="wikitable"><tr><th></th><th>Nome</th><th>Vocação</th><th>Level</th><th>Guilda</th></tr>';
+			$table = '<table class="wikitable"><tr><th></th><th>' . wfMessage('tibiahighscores-name')->text() . '</th><th>' . wfMessage('tibiahighscores-vocation')->text() . '</th><th>' . wfMessage('tibiahighscores-level')->text() . '</th><th>' . wfMessage('tibiahighscores-guild')->text() . '</th></tr>';
 			for ($i = 0;$i < intval($amount);$i++) {
 				$urlCharacter = 'https://api.tibiadata.com/v2/characters/' . str_replace(' ', '+', $highscores[$i]['name']) . '.json';
 				$json2 = file_get_contents($urlCharacter);
